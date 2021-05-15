9e0548d19967ee0a9e94df46520088ec429db98f
Javier Lanatta <jlanatta@quadiontech.com>
2021-05-15 18:59:01 -0300

    param for days

--- app/jobs/calculate_moving_average_job.rb
+++ app/jobs/calculate_moving_average_job.rb
@@ -2,11 +2,10 @@ class CalculateMovingAverageJob < ApplicationJob
   queue_as :default
 
   def perform(*_args)
-    # max_date = Stat.maximum(:date)
+    days = ENV.fetch('AVERAGE_DAYS', '7').to_i - 1
     StatSubtype.order(:key).each do |subtype|
-      Rails.logger.info("Calculating moving average for #{subtype.key}")
       subtype.stats.where(moving_average: nil).order(:date).each do |stat|
-        average = Stat.where(stat_subtype: subtype, date: stat.date - 4.days..stat.date).average(:value)
+        average = Stat.where(stat_subtype: subtype, date: stat.date - days.days..stat.date).average(:value)
         stat.update!(moving_average: average)
       end
     end
