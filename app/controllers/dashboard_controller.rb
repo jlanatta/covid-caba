d855d2a561f6e9c887b8de1b7004e02b468e40a0
Javier Lanatta <jlanatta@quadiontech.com>
2021-05-15 13:52:29 -0300

    Charts refactored

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
-  helper_method :data_for, :stats_percent_isopado, :stats_totals, :stats_fallecidos, :stats_camas
+  helper_method :data_for
   before_action :load_extras, only: %i[index retrieve_data]
 
   ## List keys using:
@@ -14,51 +14,11 @@ class DashboardController < ApplicationController
     render :index
   end
 
-  def stats_totals
-    keys = [
-      %w[personas_hisopadas personas_hisopadas_reportados_del_dia_caba],
-      %w[casos_residentes casos_confirmados_reportados_del_dia]
-    ]
-    stats_for_keys(keys)
-  end
-
-  def stats_percent_isopado
-    keys = [
-      %w[personas_hisopadas %_positividad_personas_hisopadas_reportadas_del_dia_totales]
-    ]
-    stats_for_keys(keys)
-  end
-
-  def stats_fallecidos
-    keys = [
-      %w[casos_residentes fallecidos_reportados_del_dia]
-    ]
-    stats_for_keys(keys)
-  end
-
-  def stats_camas
-    keys = [
-      %w[ocupacion_de_camas_sistema_publico graves_no_arm],
-      %w[ocupacion_de_camas_sistema_publico graves_arm]
-    ]
-    stats_for_keys(keys)
-  end
-
   private
 
   def load_extras
     @total_camas_graves = subtype_for('total_de_camas_sistema_publico', 'graves').stats.maximum(:value)
     @months = params['months']&.to_i || 2
-  end
-
-  def stats_for_keys(keys)
-    subtypes = keys.map { |type, subtype| subtype_for(type, subtype) }
-
-    subtypes.map do |subtype|
-      { name: subtype.key.titlecase,
-        data: Stat.where('date > ?', @months.months.ago).where(stat_subtype: subtype).order(:date).pluck(:date,
-                                                                                                         :value) }
-    end
   end
 
   def data_for(type, subtype)
